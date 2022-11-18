e2905767195a2b72adf1c3af7d8486588e96438c
plaskina46 <118421132+plaskina46@users.noreply.github.com>
2022-11-18 08:57:52 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        void Ymn(int x, int y)
+        {
+
+            int result = x + y;
+            Console.WriteLine($"{x} + {y} = {result}");
+        }
+        Console.WriteLine("Введиде значения x");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введиде значения y");
+        int y = Convert.ToInt32(Console.ReadLine());
+        Ymn(x, y);
+    }
+    
+   
+
+}
+
+
+
+
+
+
+
